5ffc5974e0c52735ef8f61105ee00001b27bb8d3
Yoti <realyoti@gmail.com>
2020-06-09 15:25:42 +0300

    Upload source

---
+++ nps_themes_fixer/src/main.c
@@ -0,0 +1,25 @@
+#include <psp2/kernel/threadmgr.h>
+#include <psp2/kernel/processmgr.h>
+#include <psp2/io/fcntl.h>
+#include <stdio.h>
+
+#include "debugScreen.h"
+
+#define printf psvDebugScreenPrintf
+#define ACT_PATH "tm0:npdrm/act.dat"
+
+int main(int argc, char *argv[]) {
+	psvDebugScreenInit();
+	printf("NPS Themes Fixer by Yoti\n");
+
+	SceUID fd;
+	fd = sceIoOpen(ACT_PATH, SCE_O_RDONLY, 0777);
+	if (fd < 0)
+		fd = sceIoOpen(ACT_PATH, SCE_O_WRONLY|SCE_O_CREAT, 0777);
+	sceIoClose(fd);
+
+	printf("Done\n");
+	sceKernelDelayThread(3*1000*1000);
+	sceKernelExitProcess(0);
+	return 0;
+}
